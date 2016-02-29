061de223ca4692ac99224fa3b2c7941a3d38d1e6
Uncle Cheese <aaron@silverstripe.com>
2016-02-29 21:02:08 +1300

    add temporary nav

--- summit-video-app/source/js/components/pages/App.js
+++ summit-video-app/source/js/components/pages/App.js
@@ -1,15 +1,30 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
+import { routeActions } from 'react-router-redux';
+import URL from '../../utils/url';
 class App extends React.Component {
 
 	render () {
 		return (
 		<div>
 			<h1>App</h1>
+			<a onClick={() => {
+				this.props.dispatch(routeActions.push(URL.create('featured')));;
+			}}>Featured & Popular</a> | 
+			<a onClick={() => {
+				this.props.dispatch(routeActions.push(URL.create('')));;
+			}}>All videos</a> | 
+			<a onClick={() => {
+				this.props.dispatch(routeActions.push(URL.create('events')));;
+			}}>Events</a> | 
+			<a onClick={() => {
+				this.props.dispatch(routeActions.push(URL.create('speakers')));;
+			}}>Speakers</a>
+
 			{this.props.children}
 		</div>
 		);
 	}
 }
 
-export default App;
+export default connect()(App);
